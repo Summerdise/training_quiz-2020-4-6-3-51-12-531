79c207c799104335d0740f0267d06b1238e8daf0
Summerdise <350464517@qq.com>
2020-04-07 16:01:17 +0800

    [zmc] feat: add jurge full and select function

--- src/main/java/ParkingProcess.java
+++ src/main/java/ParkingProcess.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.*;
 
 public class ParkingProcess {
     public void init(){
@@ -11,6 +12,7 @@ public class ParkingProcess {
             PreparedStatement preparedStatement =connection.prepareStatement(resetSql);
             preparedStatement.execute();
             String createSql = "CREATE TABLE total_parking(" +
+                    "id INT PRIMARY KEY" +
                     "park_name VARCHAR(8)," +
                     "total_num INT,"+
                     "now_num INT DEFAULT 0);";
@@ -55,14 +57,14 @@ public class ParkingProcess {
             connection = DbUtil.getConnection();
             statement = connection.createStatement();
             String insertTotalTableSql = "INSERT INTO total_parking(park_name,total_num,now_num)" +
-                    "VALUES (?,?,null);";
+                    "VALUES (1,?,?,null);";
             PreparedStatement preparedStatement = connection.prepareStatement(insertTotalTableSql);
             preparedStatement.setString(1,firstParking[0]);
             preparedStatement.setInt(2,Integer.valueOf(firstParking[1]));
             preparedStatement.execute();
 
             insertTotalTableSql ="INSERT INTO total_parking(park_name,total_num,now_num)" +
-                    "VALUES (?,?,null);";
+                    "VALUES (2,?,?,null);";
             preparedStatement = connection.prepareStatement(insertTotalTableSql);
             preparedStatement.setString(1,secondParking[0]);
             preparedStatement.setInt(2,Integer.valueOf(secondParking[1]));
@@ -79,4 +81,56 @@ public class ParkingProcess {
     public void updateParking(){
 
     }
+
+    public boolean isFull(int id){
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DbUtil.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT total_num,now_num FROM total_parking" +
+                    "WHERE id='"+id+"';";
+            resultSet= statement.executeQuery(sql);
+            while(resultSet.next()){
+                int total = resultSet.getInt("total_num");
+                int nowParking = resultSet.getInt("now_num");
+                return total==nowParking;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            DbUtil.releaseSource(connection,statement,resultSet);
+        }
+        return false;
+    }
+
+    public Map<Integer,String> getStatFromParkingLot(String tableName){
+        Map<Integer,String> returnMap = new HashMap<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DbUtil.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM " +tableName+";";
+            resultSet= statement.executeQuery(sql);
+            while(resultSet.next()){
+                int parkingId = resultSet.getInt("park_name");
+                String parkingNum = resultSet.getString("car_num");
+                returnMap.put(parkingId,parkingNum);
+            }
+            return returnMap;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            DbUtil.releaseSource(connection,statement,resultSet);
+        }
+        return returnMap;
+    }
+    
 }
